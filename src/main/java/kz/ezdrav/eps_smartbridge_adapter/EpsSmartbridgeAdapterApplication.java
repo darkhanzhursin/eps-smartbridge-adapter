b2b0a9728ab741770882e3fb50fa45a64952e7d5
Darkhan Zhursin <daratechn@gmail.com>
2025-09-01 22:05:00 +0500

    Initial commit: EPS SmartBridge Adapter

---
+++ src/main/java/kz/ezdrav/eps_smartbridge_adapter/EpsSmartbridgeAdapterApplication.java
@@ -0,0 +1,13 @@
+package kz.ezdrav.eps_smartbridge_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsSmartbridgeAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsSmartbridgeAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/ezdrav/eps_smartbridge_adapter/EpsSmartbridgeAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.ezdrav.eps_smartbridge_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsSmartbridgeAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
