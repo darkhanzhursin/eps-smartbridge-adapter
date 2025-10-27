9312af008095a7b44d0b2b71e140e8c39ed78744
Darkhan <zhursin_d@ezdrav.kz>
2025-10-27 17:29:36 +0500

    add builders

---
+++ src/main/java/kz/ezdrav/eps_smartbridge_adapter/webservice/factory/builders/SetDataBuilder.java
@@ -0,0 +1,16 @@
+package kz.ezdrav.eps_smartbridge_adapter.webservice.factory.builders;
+
+import kz.ezdrav.eps_smartbridge_adapter.webservice.factory.SoapMessageBuilder;
+import org.springframework.stereotype.Component;
+
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPException;
+
+@Component
+public class SetDataBuilder implements SoapMessageBuilder {
+
+    @Override
+    public void buildBody(SOAPBody soapBody, Object data) throws SOAPException {
+
+    }
+}
