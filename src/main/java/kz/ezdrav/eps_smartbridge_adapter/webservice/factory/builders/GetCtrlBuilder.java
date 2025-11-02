04b156ff7113865819f859735b0a72583ed758ac
Darkhan <zhursin_d@ezdrav.kz>
2025-11-02 15:06:19 +0500

    implemented GetCtrlBuilder

--- src/main/java/kz/ezdrav/eps_smartbridge_adapter/webservice/factory/builders/GetCtrlBuilder.java
+++ src/main/java/kz/ezdrav/eps_smartbridge_adapter/webservice/factory/builders/GetCtrlBuilder.java
@@ -3,6 +3,7 @@ package kz.ezdrav.eps_smartbridge_adapter.webservice.factory.builders;
 import kz.ezdrav.eps_smartbridge_adapter.exception.FieldMandatoryException;
 import kz.ezdrav.eps_smartbridge_adapter.model.ws.eps.GetCtrl;
 import kz.ezdrav.eps_smartbridge_adapter.model.ws.eps.ParamsDate;
+import kz.ezdrav.eps_smartbridge_adapter.model.ws.eps.ParamsDateFull;
 import kz.ezdrav.eps_smartbridge_adapter.util.KaySoapConstants;
 import kz.ezdrav.eps_smartbridge_adapter.webservice.factory.SoapMessageBuilder;
 import org.springframework.stereotype.Component;
@@ -32,20 +33,44 @@ public class GetCtrlBuilder implements SoapMessageBuilder {
     private void buildParams(SOAPElement element, ParamsDate params) throws SOAPException {
         SOAPElement paramsElement = element.addChildElement(KaySoapConstants.PARAMS, KaySoapConstants.KAY);
 
+        // Check if this is a ParamsDateFull for extended fields
+        ParamsDateFull paramsFull = params instanceof ParamsDateFull ? (ParamsDateFull) params : null;
+
+        // RequestID (extended field, required in SOAP)
+        if (paramsFull != null && paramsFull.getRequestID() != null) {
+            addElement(paramsElement, KaySoapConstants.REQUEST_ID, paramsFull.getRequestID());
+        }
+
+        // DateBegin (required)
         var dateBegin = Optional.of(params.getDateBegin())
                 .orElseThrow(() -> new FieldMandatoryException(KaySoapConstants.DATE_BEGIN + template));
-        SOAPElement dateBeginElement = paramsElement.addChildElement(KaySoapConstants.DATE_BEGIN, KaySoapConstants.KAY);
-        dateBeginElement.addTextNode(dateBegin.toString());
+        addElement(paramsElement, KaySoapConstants.DATE_BEGIN, dateBegin);
 
+        // DateEnd (required)
         var dateEnd = Optional.of(params.getDateEnd())
-                .orElseThrow(() -> new FieldMandatoryException(KaySoapConstants.DATE_BEGIN + template));
-        SOAPElement dateEndElement = paramsElement.addChildElement(KaySoapConstants.DATE_END, KaySoapConstants.KAY);
-        dateEndElement.addTextNode(dateEnd.toString());
+                .orElseThrow(() -> new FieldMandatoryException(KaySoapConstants.DATE_END + template));
+        addElement(paramsElement, KaySoapConstants.DATE_END, dateEnd);
 
+        // TypeRegistr (required)
         var typeRegister = Optional.of(params.getTypeRegistr())
                 .orElseThrow(() -> new FieldMandatoryException(KaySoapConstants.TYPE_REGISTR + template));
-        SOAPElement typeRegisterElement = paramsElement.addChildElement(KaySoapConstants.TYPE_REGISTR, KaySoapConstants.KAY);
-        typeRegisterElement.addTextNode(typeRegister.toString());
+        addElement(paramsElement, KaySoapConstants.TYPE_REGISTR, typeRegister);
 
+        // Extended optional fields
+        if (paramsFull != null) {
+            addElement(paramsElement, KaySoapConstants.FINANCE_SOURCE, paramsFull.getFinanceSource());
+            addElement(paramsElement, KaySoapConstants.CUSTOMER_ID, paramsFull.getCustomerID());
+            addElement(paramsElement, KaySoapConstants.PERFORMER_ID, paramsFull.getPerformerID());
+            addElement(paramsElement, KaySoapConstants.TREATMENT_REASON_ID, paramsFull.getTreatmentReasonID());
+            addElement(paramsElement, KaySoapConstants.SERVICE_CDS_KIND, paramsFull.getServiceCDSKind());
+            addElement(paramsElement, KaySoapConstants.PATIENT_IIN, paramsFull.getPatientIIN());
+        }
+    }
+
+    private void addElement(SOAPElement parent, String elementName, Object value) throws SOAPException {
+        SOAPElement child = parent.addChildElement(elementName, KaySoapConstants.KAY);
+        if (value != null) {
+            child.addTextNode(value.toString());
+        }
     }
 }
